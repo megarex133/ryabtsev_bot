1676e104c8cad9b10e74827334d03305a16b7ca2
megarex133 <71461604+megarex133@users.noreply.github.com>
2022-06-29 16:23:03 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const { Telegraf } = require("telegraf"); // import telegraf.js
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const bot = new Telegraf("5557161585:AAHQmcXO-r5iEr6HJnT36BmnkyVOJiVmpUQ");
 
 //Start command
 bot.start((ctx) => {
-  if (ctx.from.username === "@sane4_kaaaaaa" || ctx.from.first_name === "@rilya1")
-  ctx.reply(
-    `Привет, ${ctx.from.first_name}\nЯ бот, разработанный Ильей для того, чтобы тебе было не так грустно в его отсутствие:)\nТы можешь со мной пообщаться и посмотреть мои функции`
-  );
-  ctx.reply(
-    "Вот список команд, которые я выполняю:\n /compliment - Сгенерировать твои любимые нежности"
-  );
+    ctx.reply(
+      `Привет, ${ctx.from.first_name}\nЯ бот, разработанный Ильей для того, чтобы тебе было не так грустно в его отсутствие:)\nТы можешь со мной пообщаться и посмотреть мои функции`
+    );
+    ctx.reply(
+      "Вот список команд, которые я выполняю:\n /compliment - Сгенерировать твои любимые нежности"
+    );
 });
 
 //Help command
