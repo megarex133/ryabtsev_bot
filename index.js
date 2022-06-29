6acc1713ff2d1246d28f6fd31274b36dd8c8eea3
megarex133 <71461604+megarex133@users.noreply.github.com>
2022-06-29 16:39:23 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const { Telegraf } = require("telegraf"); // import telegraf.js
 
 const bot = new Telegraf("5557161585:AAHQmcXO-r5iEr6HJnT36BmnkyVOJiVmpUQ");
 
+const compliments = ["Я тебя люблю❤", "Я тебя безумно люблю, малыш❤❤", "Ты мое маленькое солнышко", "Красоточкааа", "Сашенька, ты невероятно красива"];
+function generateInteger(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 //Start command
 bot.start((ctx) => {
     ctx.reply(
@@ -20,8 +26,7 @@ if (ctx.from.first_name == "Илья") {
   });
 }
 bot.command("compliment", (ctx) => {
-  ctx.reply("Введи номер учебной группы");
- 
+  ctx.reply(compliments[generateInteger]);
 });
 // Run the bot
 bot.launch();
