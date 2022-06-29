d6893ccd14fa844046dbb9bf1e6d9e16f9c18fba
megarex133 <71461604+megarex133@users.noreply.github.com>
2022-06-29 17:14:08 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const { Telegraf } = require("telegraf"); // import telegraf.js
 
 const bot = new Telegraf("5557161585:AAHQmcXO-r5iEr6HJnT36BmnkyVOJiVmpUQ");
 
-// // const compliments = ["Я тебя люблю❤", "Я тебя безумно люблю, малыш❤❤", "Ты мое маленькое солнышко", "Красоточкааа", "Сашенька, ты невероятно красива"];
-// // function generateInteger(min, max) {
-// //   min = Math.ceil(min);
-// //   max = Math.floor(max);
-// //   return Math.floor(Math.random() * (max - min + 1) + min);
-// }
+const compliments = ["Я тебя люблю❤", "Я тебя безумно люблю, малыш❤❤", "Ты мое маленькое солнышко", "Красоточкааа", "Сашенька, ты невероятно красива"];
+function generateInteger(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 //Start command
 bot.start((ctx) => {
     ctx.reply(
@@ -17,16 +17,14 @@ bot.start((ctx) => {
       "Вот список команд, которые я выполняю:\n /compliment - Сгенерировать твои любимые нежности"
     );
 });
-if (ctx.from.first_name == "Илья") {
   //Help command
   bot.help((ctx) => {
     ctx.reply(
       "Вот список команд, которые я выполняю:\n /compliment - Сгенерировать твои любимые нежности"
     );
   });
-}
 bot.command("compliment", (ctx) => {
-  // ctx.reply(compliments[generateInteger]);
+  ctx.reply(compliments[generateInteger]);
 });
 // Run the bot
 bot.launch();
